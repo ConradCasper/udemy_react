ce6e83d398282ab27678159f6e28c08784668b7d
Conrad <cnrdcasper@gmail.com>
2022-05-15 13:27:07 -0400

    not sure what changes ive made but im committing them anyway

--- src/App.js
+++ src/App.js
@@ -33,11 +33,19 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximillian')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Maximillian')}>Switch Name</button>
         <Person click={this.switchNameHandler.bind(this, 'Maxi poo')} name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person changed={this.nameChangedHandler} name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
